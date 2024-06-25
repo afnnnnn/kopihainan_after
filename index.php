fddf1677445e1f0e32a4e37bb05cedc747d650c5
afnnnnn <103879224+afnnnnn@users.noreply.github.com>
2024-06-25 13:20:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
   <?php
     // Set the Content-Security-Policy header(only allow scripts from the same domain)
     header("Content-Security-Policy: script-src 'self'");
-
+  
     // Start the session
     session_start();
+
+    // Check if the user is logged in
+    if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+      header("Location: login.php");
+      exit;
+  }
 
     // Get the session ID
     $sessionId = session_id();
@@ -29,11 +35,12 @@
     <header>
       <a href="index.html" class="logo"><img src="img/lg-khainan.png"></a>
       <ul>
-        <li><a href = "index.html">Home</a></li>
-        <li><a href = "product.html">What we sell</a></li>
+        <li><a href = "index.php">Home</a></li>
+        <li><a href = "product.php">What we sell</a></li>
         <li><a href = "promotion.html">Promotion</a></li>
-        <li><a href = "about.html">About Us</a></li>
-        <li><a href = "contact.html">Contact</a></li>
+        <li><a href = "about.php">About Us</a></li>
+        <li><a href = "contact.php">Contact</a></li>
+
       </ul>
     </header>
     <div class = "content">
@@ -47,6 +54,31 @@
 
   </section>
 
+  <style>
+    /* Add your custom styles here */
+    .logout-button {
+      background-color: #ff6f00;
+      color: #fff;
+      padding: 10px 20px;
+      border: none;
+      border-radius: 5px;
+      font-size: 16px;
+      cursor: pointer;
+      display: block;
+      margin: 0 auto;
+      width: 200px;
+      text-align: center;
+    }
+
+    .logout-button:hover {
+      background-color: #ff8f00;
+    }
+  </style>
+
+  <form method="post" action="logout.php">
+    <button type="submit" class="logout-button">Logout</button>
+  </form>
+  
   <div class="footer">
       <div class="social-footer">
         <a href="https://www.instagram.com/kopihainangombak/" target="_blank"><i class="fab fa-instagram"></i></a>
@@ -56,19 +88,19 @@
 
       <ul class="list">
         <li>
-          <a href="index.html">Home</a>
+          <a href="index.php">Home</a>
         </li>
         <li>
-          <a href="product.html">What we sell</a>
+          <a href="product.php">What we sell</a>
         </li>
         <li>
           <a href="promotion.html">Promotion</a>
         </li>
         <li>
-          <a href="about.html">About Us</a>
+          <a href="about.php">About Us</a>
         </li>
         <li>
-          <a href="contact.html">Contact</a>
+          <a href="contact.php">Contact</a>
         </li>
       </ul>
       <p class="copyright">
